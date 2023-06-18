d23141dcf322ff2269b8c3eb8e9c43d6850f361d
anUnnecessary <raihanshamil33@gmail.com>
2023-06-18 15:40:18 +0600

    profile added

---
+++ src/app/profile/page.jsx
@@ -0,0 +1,10 @@
+export const metadata = {
+    title: "Profile - Easy Shop",
+  };
+  
+  const ProfilePage = () => {
+    return <div>ProfilePage</div>;
+  };
+  
+  export default ProfilePage;
+  
